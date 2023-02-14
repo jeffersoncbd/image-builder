63449bdbcc256ea3893e02b7d2a89ef14255b008
Jefferson Carlos <jeff.carlosbd@gmail.com>
2023-02-14 14:18:16 -0300

    implementa novas cores

--- src/colors.rs
+++ src/colors.rs
@@ -3,21 +3,29 @@ use std::collections::HashMap;
 const YELLOW: [u8; 4] = [255, 255, 0, 255];
 const GREEN: [u8; 4] = [0, 176, 80, 255];
 const GRAY: [u8; 4] = [191, 191, 191, 255];
+const RED: [u8; 4] = [255, 0, 0, 255];
+const BLUE: [u8; 4] = [0, 112, 192, 255];
+const ORANGE: [u8; 4] = [255, 153, 0, 255];
+const VIOLET: [u8; 4] = [112, 48, 160, 255];
 
 pub struct Colors<'a, 'b> {
-  colors: HashMap<&'b str, &'a[u8; 4]>
+    colors: HashMap<&'b str, &'a [u8; 4]>,
 }
 impl<'a, 'b> Colors<'a, 'b> {
-  pub fn new() -> Colors<'a, 'b> {
-    Colors {
-      colors: HashMap::from([
-        ("yellow", &YELLOW),
-        ("green", &GREEN),
-        ("gray", &GRAY),
-      ])
+    pub fn new() -> Colors<'a, 'b> {
+        Colors {
+            colors: HashMap::from([
+                ("yellow", &YELLOW),
+                ("green", &GREEN),
+                ("gray", &GRAY),
+                ("red", &RED),
+                ("blue", &BLUE),
+                ("orange", &ORANGE),
+                ("violet", &VIOLET),
+            ]),
+        }
     }
-  }
-  pub fn get(&self, name: &str) -> Option<&[u8; 4]> {
-    self.colors.get(name).copied()
-  }
+    pub fn get(&self, name: &str) -> Option<&[u8; 4]> {
+        self.colors.get(name).copied()
+    }
 }
